86eee7bc4f2319a7573da2e391bf1580ade840cc
root <root@localhost>
2017-03-30 15:57:56 -0400

    javascript

---
+++ js/clock/clock.js
@@ -0,0 +1,17 @@
+function setup() {
+	createCanvas(windowWidth,windowheight);
+}
+
+function draw() {
+	background(50,150,100);
+	
+	var d = day();
+	var m = month();
+	var y = year();
+	var h = hour();
+	var min = minute();
+	var sec = second();
+
+	text("date is " + m + " " + d + " " + y + " " , 5, 40):
+	text("time is " + h + " " + min + " " + sec + " ", 5, 50);
+}
